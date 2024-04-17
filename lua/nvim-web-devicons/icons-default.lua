c75f0e1c3918196602e98ac23c70d2a63769092f
yamadote <yamadote@gmail.com>
2024-04-17 21:06:59 +0300

    Add php icon

--- lua/nvim-web-devicons/icons-default.lua
+++ lua/nvim-web-devicons/icons-default.lua
@@ -1,17 +1,14 @@
-local icons_by_filename = {
-}
+local purple = "#a074c4"
+
+local icons_by_filename = {}
 
 local icons_by_file_extension = {
+  ["php"] = { icon = "", color = purple, cterm_color = "140", name = "Php" },
 }
 
-local icons_by_operating_system = {
-}
-
-local icons_by_desktop_environment = {
-}
-
-local icons_by_window_manager = {
-}
+local icons_by_operating_system = {}
+local icons_by_desktop_environment = {}
+local icons_by_window_manager = {}
 
 return {
   icons_by_filename = icons_by_filename,
