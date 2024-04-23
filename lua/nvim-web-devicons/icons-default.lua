be4bb3459592645cfc22c3e41135c24bb44c07f6
yamadote <yamadote@gmail.com>
2024-04-23 13:54:47 +0300

    Add icons

--- lua/nvim-web-devicons/icons-default.lua
+++ lua/nvim-web-devicons/icons-default.lua
@@ -22,20 +22,28 @@ local function red(opts)
   return opts
 end
 
+local function orange(opts)
+  opts.color = "#e37933"
+  opts.cterm_color = "166"
+  return opts
+end
+
 local tag_icon = "󰗀"
 local image_icon = ""
 local cog_icon = ""
+local hash_icon = ""
 
 local icons_by_file_extension = {
   ["php"] = magenta { icon = "", name = "Php" },
-  ["py"] = yellow { icon = "", name = "Py" },
+  ["py"] = yellow { icon = "", name = "Py" },
   ["go"] = cyan { icon = "", name = "Go" },
   ["lua"] = cyan { icon = "", name = "Lua" },
   ["js"] = yellow { icon = "", name = "Js" },
-  ["css"] = cyan { icon = "", name = "Css" },
-  ["scss"] = magenta { icon = "", name = "Scss" },
+  ["ts"] = cyan { icon = "", name = "Ts" },
+  ["css"] = cyan { icon = hash_icon, name = "Css" },
+  ["scss"] = red { icon = hash_icon, name = "Scss" },
   ["html"] = red { icon = tag_icon, name = "Html" },
-  ["xml"] = yellow { icon = tag_icon, name = "Xml" },
+  ["xml"] = orange { icon = tag_icon, name = "Xml" },
   ["png"] = magenta { icon = image_icon, name = "Png" },
   ["jpg"] = magenta { icon = image_icon, name = "Jpg" },
   ["webp"] = magenta { icon = image_icon, name = "Webp" },
